0e74a299a7e6663b57a0b2cd72aa9c07286a9c1e
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-07-22 02:12:23 +0530

    fix database

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,35 +5,87 @@ const createUser = async (user) => {
   const {
     customerId,
     fullName,
-    mobileNumber, 
+    mobileNumber,
     adharNumber,
     dateOfBirth,
     nationality,
-    pastalcode,
+    postalCode,
     email,
     city,
     state,
-    phonenumber,
+    phoneNumber,
     password,
-    dob,
     gender,
   } = user;
-  if(!customerId || !fullName || !mobileNumber || !dateOfBirth || !pastalcode || !adharNumber || !nationality || !email || !city || !state || !phonenumber || !password || !dob || !gender) {
+
+  if (
+    !customerId ||
+    !fullName ||
+    !mobileNumber ||
+    !adharNumber ||
+    !dateOfBirth ||
+    !nationality ||
+    !postalCode ||
+    !email ||
+    !city ||
+    !state ||
+    !phoneNumber ||
+    !password ||
+    !gender
+  ) {
     throw new Error("All fields are required");
   }
 
   const existingUser = await pool.query(
-    "SELECT * FROM users WHERE email = $1 OR phone = $2 OR adhar_number = $3",
-    [email, phonenumber, adharNumber]
+    `SELECT * FROM users
+     WHERE email = $1
+        OR phone = $2
+        OR adhar_number = $3`,
+    [email, phoneNumber, adharNumber]
   );
-  
+
+  if (existingUser.rows.length > 0) {
+    throw new Error("User already exists");
+  }
+
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const query = `
     INSERT INTO users
-    (customer_id, full_name, mobile_number, date_of_birth, postal_code, adhar_number, nationality, email, city, state, phone, password, dob, gender)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-    RETURNING id, customer_id, full_name, mobile_number, date_of_birth, postal_code, adhar_number, nationality, email, city, state, phone, dob, gender, created_at
+    (
+      customer_id,
+      full_name,
+      mobile_number,
+      date_of_birth,
+      postal_code,
+      adhar_number,
+      nationality,
+      email,
+      city,
+      state,
+      phone,
+      password,
+      gender
+    )
+    VALUES
+    (
+      $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13
+    )
+    RETURNING
+      id,
+      customer_id,
+      full_name,
+      mobile_number,
+      date_of_birth,
+      postal_code,
+      adhar_number,
+      nationality,
+      email,
+      city,
+      state,
+      phone,
+      gender,
+      created_at;
   `;
 
   const values = [
@@ -41,21 +93,22 @@ const createUser = async (user) => {
     fullName,
     mobileNumber,
     dateOfBirth,
-    pastalcode,
+    postalCode,
     adharNumber,
     nationality,
     email,
     city,
     state,
-    phonenumber,
+    phoneNumber,
     hashedPassword,
-    dob,
     gender,
   ];
+
   const result = await pool.query(query, values);
 
   return result.rows[0];
 };
+
 module.exports = {
   createUser,
 };
