6e3ca5818dad9b2326d0d205dbd76a1aa23d2fb3
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-07-22 23:32:08 +0530

    correct the code

--- src/controllers/auth.controler.js
+++ src/controllers/auth.controler.js
@@ -11,7 +11,7 @@ function registerUser(req, res) {
       res.status(400).json({ error: error.message });
     });
 }
-funtion loginUser(req,res){
+function loginUser(req,res){
   const { email, password } = req.body;
   userModel.loginUser(email, password)
     .then((user) => {
